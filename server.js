fcb806e386c699a3f3640280c7ef92df514f44f8
max <shadymaxwell@gmail.com>
2015-02-21 14:53:07 -0800

    adding nodejs server API magic goodness

---
+++ server.js
@@ -0,0 +1,41 @@
+var http = require('http')
+var port = process.env.PORT || 1337;
+var express = require('express');
+var app = express(); //init
+
+///////////////////////////////////////////
+// Hackster IO Hackathon! 
+
+var rider_waiting = false;
+var driver_incoming = false;
+
+app.get('/hack/requestride', function (req, res) {
+    // mobile app requests a ride
+
+    // TODO: make this pose, accept name and date and address
+
+    rider_waiting = true;
+    res.send('{}');
+});
+app.get('/hack/rideavailable', function (req, res) {
+    // edison pings, asking if rider is waiting
+    driver_incoming = true;
+    res.send('{result:"' + rider_waiting + '", ' +
+        'address:"'+'123 fake st'+'",' +
+        'name:"'+'Marty McFly'+'",' +
+        'date:"'+'1973'+'",' +
+        'weather:"'+'Sunny'+'",' +
+        '}');
+});
+app.get('/hack/rideaccepted', function (req, res) {
+    // driver pushed button on edison
+    res.send('{}');
+});
+app.get('/hack/rideprogress', function (req, res) {
+    // mobile app wants to know where driver is
+    res.send('{result:' + driver_incoming + ', ' +
+        'carlocation:"999 3rd ave"' +
+        'cardate:"2015"' +
+        '}');
+});
+
