8c260f194dfdfbd1a499bf4c222201bdaa7f7c45
Surya <ayrus@Suryas-MacBook.local>
2015-02-21 17:14:34 -0800

    adding weather services

--- server.js
+++ server.js
@@ -2,37 +2,100 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 var express = require('express');
 var app = express(); //init
+var bodyParser = require('body-parser')
+
+
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 ///////////////////////////////////////////
 // Hackster IO Hackathon! 
 
 var rider_waiting = false;
 var driver_incoming = false;
+var rider_id = 1;
+var rider_details = [];
 
 app.get('/', gethome);
+
+
+function findWeatherAndReturn(rider ,response){
+    path = "http://api.wunderground.com/api/73de21f5b11ba67d/history_";
+    path += (rider.date +  "/q/" + rider.state + "/" + rider.city + ".json")
+
+    console.log(path)
+
+    //do the http request to get the weather
+    var options = {
+        hostname: 'api.wunderground.com',
+        path: path
+    };
+
+    http.get(path, function (http_res) {
+        var data = "";
+
+        // this event fires many times, each time collecting another piece of the response
+        http_res.on("data", function (chunk) {
+            // append this chunk to our growing `data` var
+            data += chunk;
+        });
+
+        // this event fires *one* time, after all the `data` events/chunks have been gathered
+        http_res.on("end", function () {
+            // you can use res.send instead of console.log to output via express
+            console.log(data);
+            data = JSON.parse(data)
+            weather = data['history']['dailysummary'][0]['meantempi']
+            
+            rider['weather'] = weather
+            response.send(rider);
+        });
+    });
+}
+
 function gethome(req, res) {
     res.send('<html><body><h1>Hackster IO -- Tuber App!</h1><br/>' +
         '');
 }
 
-app.get('/hack/requestride', function (req, res) {
+app.post('/hack/requestride', function (req, res) {
     // mobile app requests a ride
 
     // TODO: make this pose, accept name and date and address
-
+    if (!req.body) return res.sendStatus(400)
     rider_waiting = true;
-    res.send('{}');
+
+    var id = rider_id;
+    rider_id++;
+
+    rider = {
+        'name' : req.body.name,
+        'date' : req.body.date,
+        'address' : req.body.address,
+        'state' : req.body.state,
+        'city': req.body.city
+
+    }
+
+    rider_details.push( rider )
+    res.send('welcome, ' + req.body.name)
 });
+
 app.get('/hack/rideavailable', function (req, res) {
     // edison pings, asking if rider is waiting
     driver_incoming = true;
-    res.send('{result:"' + rider_waiting + '", ' +
-        'address:"'+'123 fake st'+'",' +
-        'name:"'+'Marty McFly'+'",' +
-        'date:"'+'1973'+'",' +
-        'weather:"'+'Sunny'+'",' +
-        '}');
+
+    //find the weather.
+
+    rider =  rider_details.pop()
+
+    weather = findWeatherAndReturn(rider, res)
+    
+
 });
+
 app.get('/hack/rideaccepted', function (req, res) {
     // driver pushed button on edison
     res.send('{}');
@@ -45,3 +108,9 @@ app.get('/hack/rideprogress', function (req, res) {
         '}');
 });
 
+var server = app.listen(3000, function() {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('Example app listening at http://%s:%s', host, port)
+});
